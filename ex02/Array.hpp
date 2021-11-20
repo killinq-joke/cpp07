1666dd3351919a20a38d2dc5b07cda5a317fd2a6
Zakaria Touzri <ztouzri@c1r2p8.42nice.fr>
2021-11-20 13:41:01 +0100

    mvp

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: ztouzri <ztouzri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 17:27:07 by mout              #+#    #+#             */
-/*   Updated: 2021/11/20 13:18:03 by ztouzri          ###   ########.fr       */
+/*   Updated: 2021/11/20 13:40:35 by ztouzri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,39 @@ class Array
 {
 
 public:
-    Array(void) { this->_arr = NULL; this->_n = 0; };
-    Array(unsigned int n) { this->_arr = new T[n]; this->_n = n; };
+    Array(void) { this->_arr = nullptr; this->_n = 0; };
+    Array(unsigned int n)
+	{
+		if (n)
+			this->_arr = new T[n];
+		else
+			this->_arr = nullptr;
+		this->_n = n;
+	};
     Array(Array const & a1) { *this = a1; };
 	Array operator=(Array const & a1)
 	{
-		if (this->_arr) delete [] this->_arr; this->_arr = new T[a1.size()];
+		if (this->_arr != nullptr && this->size())
+		{
+			std::cout << this->size() << std::endl;
+			delete [] this->_arr;
+			this->_arr = nullptr;
+		}
+		this->_arr = new T[a1.size()];
 		for (unsigned int i = 0; i < a1.size(); i++)
 		{
 			this->_arr[i] = a1[i];
 		}
 		return (*this);
 	};
-    ~Array(void) { if (this->_arr) delete [] this->_arr; };
+    ~Array(void)
+	{
+		std::cout << "Array Destructor" << std::endl;
+		if (this->_arr != nullptr)
+			delete [] this->_arr;
+		this->_arr = nullptr;
+	};
+
 	T	& operator[](unsigned int i)
 	{ 
 			if (i >= this->size())
@@ -53,6 +73,7 @@ public:
 
 	class OutOfLimits : public std::exception
 	{
+	public:
 		const char *what() const throw()
 		{
 			return ("Index out of limits");
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ztouzri <ztouzri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 16:52:39 by mout              #+#    #+#             */
-/*   Updated: 2021/11/20 11:49:45 by ztouzri          ###   ########.fr       */
+/*   Updated: 2021/11/20 13:28:15ztouzri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,34 @@
 
 int main(void)
 {
-	Array<int> a = Array<int>(10);
-
-	a[10] = 2;
-    return (0);
+	Array<int>int_arr(21);
+	Array<float>f_arr(21);
+	int i = 0;
+	while ((unsigned int)i < 22)
+	{
+		try
+		{
+			int_arr[i] = i;
+			std::cout << "int: " << int_arr[i] << std::endl;
+		}
+		catch (Array<int>::OutOfLimits & ex)
+		{
+			std::cout << ex.what() << std::endl;
+		}
+		i++;
+	}
+	i = 0;
+	while ((unsigned int)i < 22)
+	{
+		try
+		{
+			f_arr[i] = i + 0.1;
+			std::cout << "float: " << f_arr[i] << std::endl;
+		}
+		catch (Array<float>::OutOfLimits & ex)
+		{
+			std::cout << ex.what() << std::endl;
+		}
+		i++;
+	}
 }
